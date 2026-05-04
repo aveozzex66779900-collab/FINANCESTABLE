5f10d654b0ca2b81bcd2b3aacfed1c8c4b451554
Shanu Kumar <aveozzex66779900@gmail.com>
2026-05-04 23:52:16 +0530

    fix isolated auth safely

--- frontend/js/auth-safe.js
+++ frontend/js/auth-safe.js
@@ -1,11 +1,7 @@
 console.log("✅ AUTH SAFE LOADED");
 
 
-const API =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
-    ? "http://localhost:5000"
-    : "https://financestable.onrender.com";
+
 
 
 // =======================
